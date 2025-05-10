7a4086eb1a1f1ad7965e48edc9151376b905a509
JeanVittory <vittory.dev@gmail.com>
2025-05-10 12:49:39 -0500

    feat: :sparkles: Add the add todo and remove todo service to interact with the backend

--- src/services/todos/index.ts
+++ src/services/todos/index.ts
@@ -1,9 +1,37 @@
 import axios from "axios";
-import { BASE_URL, GET_USER_TODOS_ENDPOINT } from "../../constants";
+import {
+  BASE_URL,
+  GET_USER_TODOS_ENDPOINT,
+  POST_TODO_ENDPOINT,
+  DELETE_TODO_ENDPOINT,
+} from "../../constants";
+import type { CreateTodo } from "../../types/todos";
 
-export const getTodosByUserEmail = async () => {
+export const getTodos = async () => {
   const response = await axios.get(`${BASE_URL}${GET_USER_TODOS_ENDPOINT}`, {
     withCredentials: true,
   });
+  if (response.status === 401) throw response.statusText;
+  return response.data.response;
+};
+
+export const addTodo = async (newTodo: CreateTodo) => {
+  const response = await axios.post(
+    `${BASE_URL}${POST_TODO_ENDPOINT}`,
+    newTodo,
+    {
+      withCredentials: true,
+    }
+  );
+  return response.data.response;
+};
+
+export const removeTodo = async (todoId: number) => {
+  const response = await axios.delete(
+    `${BASE_URL}${DELETE_TODO_ENDPOINT}/${todoId}`,
+    {
+      withCredentials: true,
+    }
+  );
   return response.data.response;
 };
