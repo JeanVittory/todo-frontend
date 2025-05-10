1b074576596ca27a95e069307fa43ee1dd4529a5
JeanVittory <vittory.dev@gmail.com>
2025-05-10 12:50:58 -0500

    feat: :sparkles: Add TodoForm and TodoList component into Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,31 +1,75 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { useAuthStore } from "../store";
-import { getTodosByUserEmail } from "../services/todos";
-import type { Todo } from "../types/todos";
+import { addTodo, getTodos, removeTodo } from "../services/todos";
+import type { CreateTodo, Todo } from "../types/todos";
+import { useAuth } from "../providers/AuthProvider";
+import { useNavigate } from "react-router";
+import TodoForm from "../components/TodoForm";
+import TodoList from "../components/todoList";
+import { handleApiError } from "../utils";
 
 export default function Dashboard() {
+  const [todos, setTodos] = useState<Todo[]>([]);
   const user = useAuthStore((state) => state.getCurrentUser());
-  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const { signOut } = useAuth();
+  const navigate = useNavigate();
+  const clearUser = useAuthStore((state) => state.clearUser);
 
   useEffect(() => {
-    getTodosByUserEmail().then((response: Todo[]) => {
-      setTodos(response);
-    });
-  }, []);
-  console.log(user);
+    getTodos()
+      .then((response: Todo[]) => {
+        setTodos(response);
+      })
+      .catch(() => {
+        console.log("me ejecute 1");
+
+        signOut();
+        clearUser();
+        navigate("/", { replace: true });
+      });
+  }, [navigate, clearUser, signOut]);
+
+  if (!user || !Object.keys(user).length) {
+    console.log("me ejecute 2");
+    signOut();
+    clearUser();
+    navigate("/", { replace: true });
+  }
+
+  const handleAddTodo = async (newTodo: CreateTodo) => {
+    try {
+      const todoAdded = await addTodo(newTodo);
+      setTodos((prev) => [...prev, todoAdded]);
+    } catch (error) {
+      handleApiError(error);
+    }
+  };
+  const handleRemoveTodo = async (todoId: number) => {
+    try {
+      const newTodoList = await removeTodo(todoId);
+      console.log(newTodoList);
+
+      setTodos(newTodoList);
+    } catch (error) {
+      handleApiError(error);
+    }
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Header />
-      {/* Main Content */}
       <main className="flex-1 container mx-auto px-4 py-6">
-        <div className="bg-white rounded-lg shadow p-6">
-          <h1 className="text-2xl font-bold mb-4">Bienvenido a tu Dashboard</h1>
-          <p className="text-gray-600">
-            Este es el contenido principal de tu dashboard. Aquí puedes mostrar
-            estadísticas, gráficos, tablas o cualquier otra información
-            relevante para tu aplicación.
-          </p>
+        <h1 className="text-2xl font-bold mb-6">Gestor de Tareas</h1>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <div>
+            <TodoForm onAddTodo={handleAddTodo} />
+          </div>
+
+          <div>
+            <TodoList todos={todos} handleRemoveTodo={handleRemoveTodo} />
+          </div>
         </div>
       </main>
     </div>
