598aac0a4babffcd03569d9227bc69e40ef6a1db
JeanVittory <vittory.dev@gmail.com>
2025-05-10 12:50:03 -0500

    feat: :sparkles: Add the createTodo type

--- src/types/todos.ts
+++ src/types/todos.ts
@@ -1,8 +1,15 @@
-type Priority = "low" | "medium" | "high";
+export type Priority = "low" | "medium" | "high";
 
 export type Todo = {
   id: number;
   userId: number;
+  title: string;
+  description: string;
+  completed: boolean;
+  priority: Priority;
+};
+
+export type CreateTodo = {
   title: string;
   description: string;
   completed: boolean;
