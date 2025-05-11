c680833a93296e68c1a566a0f0959b1c01dfe1fa
JeanVittory <vittory.dev@gmail.com>
2025-05-11 01:36:18 -0500

    feat: :wrench: Add a new endpoint into the constants file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,10 +2,12 @@ export const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const INVALID_EMAIL_MESSAGE = "Please use a valid email.";
 export const PASSWORD_REQUIRED_MESSAGE = "Password is required.";
 
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 export const PUBLIC_JWT_KEY = import.meta.env.VITE_PUBLIC_JWT_KEY;
+
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 export const LOGIN_ENDPOINT = "/login";
 export const LOGOUT_ENDPOINT = "/logout";
 export const GET_USER_TODOS_ENDPOINT = "/todos";
 export const POST_TODO_ENDPOINT = "/todo";
 export const DELETE_TODO_ENDPOINT = "/todo";
+export const VERIFY_ENDPOINT = "/verify";
