d85450bed118e52404e38c785dc6f5ee3f465c8d
JeanVittory <vittory.dev@gmail.com>
2025-05-11 21:38:40 -0500

    feat: :art: Add some messages and UI text

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,40 +1,27 @@
 import { ListChecks, Trash2 } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { Checkbox } from "./ui/checkbox";
 import { Badge } from "./ui/badge";
-import type { Priority, Todo } from "../types/todos";
+import type { Todo } from "../types/todos";
+import { getPriorityColor } from "../utils";
 
-interface TodoListProps {
+type TodoListProps = {
   todos: Todo[];
   handleRemoveTodo: (todoId: number) => void;
-}
+};
 
 export default function TodoList({ todos, handleRemoveTodo }: TodoListProps) {
-  const getPriorityColor = (priority: Priority) => {
-    switch (priority) {
-      case "low":
-        return "bg-blue-100 text-blue-800 hover:bg-blue-100";
-      case "medium":
-        return "bg-yellow-100 text-yellow-800 hover:bg-yellow-100";
-      case "high":
-        return "bg-red-100 text-red-800 hover:bg-red-100";
-      default:
-        return "";
-    }
-  };
-
   return (
     <Card>
       <CardHeader>
         <CardTitle className="text-xl flex items-center gap-2">
           <ListChecks className="h-5 w-5" />
-          Mis Tareas
+          My Tasks
         </CardTitle>
       </CardHeader>
       <CardContent>
         {!todos || todos.length === 0 ? (
           <p className="text-center text-gray-500 py-4">
-            No hay tareas pendientes
+            There is not pending tasks
           </p>
         ) : (
           <ul className="space-y-4">
@@ -42,7 +29,6 @@ export default function TodoList({ todos, handleRemoveTodo }: TodoListProps) {
               todos.map((todo) => (
                 <li key={todo.id} className="border rounded-lg p-4">
                   <div className="flex items-start gap-3">
-                    <Checkbox checked={todo.completed} className="mt-1" />
                     <div className="flex-1">
                       <div className="flex items-center justify-between">
                         <h3
@@ -59,6 +45,7 @@ export default function TodoList({ todos, handleRemoveTodo }: TodoListProps) {
                           <Trash2
                             size="20px"
                             onClick={() => handleRemoveTodo(todo.id)}
+                            className="lg:cursor-pointer"
                           />
                         </div>
                       </div>
