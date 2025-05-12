7f699a4c16bbeae17b11f9d04717866e26dd7177
JeanVittory <vittory.dev@gmail.com>
2025-05-11 21:36:42 -0500

    feat: :sparkles: Add a Not Found component

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,3 +1,7 @@
 export default function NotFound() {
-  return <div>Page 404 | Page not found</div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      Page 404 | Page not found
+    </div>
+  );
 }
