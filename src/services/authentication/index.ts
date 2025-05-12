47fe4a6253a75c4cec007a8a5c8fa5bcd75913cc
JeanVittory <vittory.dev@gmail.com>
2025-05-11 21:40:02 -0500

    feat: :sparkles: Add the api version into  the request url

--- src/services/authentication/index.ts
+++ src/services/authentication/index.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
-import { BASE_URL, LOGOUT_ENDPOINT, LOGIN_ENDPOINT } from "../../constants";
+import {
+  BASE_URL,
+  LOGOUT_ENDPOINT,
+  LOGIN_ENDPOINT,
+  API_V1,
+} from "../../constants";
 import type { Credentials } from "../../types/credentials";
 
 export const authenticate = async (credentials: Credentials) => {
   const response = await axios.post(
-    `${BASE_URL}${LOGIN_ENDPOINT}`,
+    `${BASE_URL}${API_V1}${LOGIN_ENDPOINT}`,
     credentials,
     { withCredentials: true }
   );
@@ -13,7 +18,7 @@ export const authenticate = async (credentials: Credentials) => {
 
 export const logout = async () => {
   const response = await axios.post(
-    `${BASE_URL}${LOGOUT_ENDPOINT}`,
+    `${BASE_URL}${API_V1}${LOGOUT_ENDPOINT}`,
     {},
     { withCredentials: true }
   );
--- src/services/todos/index.ts
+++ src/services/todos/index.ts
@@ -4,20 +4,24 @@ import {
   GET_USER_TODOS_ENDPOINT,
   POST_TODO_ENDPOINT,
   DELETE_TODO_ENDPOINT,
+  API_V1,
 } from "../../constants";
 import type { CreateTodo } from "../../types/todos";
 
 export const getTodos = async () => {
-  const response = await axios.get(`${BASE_URL}${GET_USER_TODOS_ENDPOINT}`, {
-    withCredentials: true,
-  });
+  const response = await axios.get(
+    `${BASE_URL}${API_V1}${GET_USER_TODOS_ENDPOINT}`,
+    {
+      withCredentials: true,
+    }
+  );
   if (response.status === 401) throw response.statusText;
   return response.data.response;
 };
 
 export const addTodo = async (newTodo: CreateTodo) => {
   const response = await axios.post(
-    `${BASE_URL}${POST_TODO_ENDPOINT}`,
+    `${BASE_URL}${API_V1}${POST_TODO_ENDPOINT}`,
     newTodo,
     {
       withCredentials: true,
@@ -28,7 +32,7 @@ export const addTodo = async (newTodo: CreateTodo) => {
 
 export const removeTodo = async (todoId: number) => {
   const response = await axios.delete(
-    `${BASE_URL}${DELETE_TODO_ENDPOINT}/${todoId}`,
+    `${BASE_URL}${API_V1}${DELETE_TODO_ENDPOINT}/${todoId}`,
     {
       withCredentials: true,
     }
